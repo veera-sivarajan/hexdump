066a065926fc3a26a9a2783de3b33c2ee3d65af0
Veera <sveera.2001@gmail.com>
2022-10-08 17:31:32 -0400

    Complete implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,55 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
+use std::fmt::Write;
 
 #[derive(Debug)]
 pub enum HexdumpError {
     PathIsDir,
     FileOpen,
     ReadBuffer,
+    WriteBuffer,
 }
 
 type Result<T> = std::result::Result<T, HexdumpError>;
 
 trait HexPrinter {
-    fn print(&self, count: usize);
+    fn print(&self, count: usize) -> Result<()>;
 }
 
 impl HexPrinter for &[u8] {
-    fn print(&self, count: usize) {
-        print!("{:08x} ", count);
+    fn print(&self, count: usize) -> Result<()> {
+        print!("{:08x}  ", count);
+        let mut ascii_buf = String::new();
         for (count, byte) in self.iter().enumerate() {
             if count == 8 {
                 print!(" ");
             }
             
+            if *byte >= 32 && *byte <= 126 {
+                write!(ascii_buf, "{}", *byte as char).map_err(|_err| HexdumpError::WriteBuffer)?;
+            } else {
+                write!(ascii_buf, ".").map_err(|_err| HexdumpError::WriteBuffer)?;
+            }
+            
             print!("{:02x} ", byte);
         }
-        println!();
+        
+        let mut len = self.len();
+        while len < 16 { 
+            if len == 8 {
+                print!(" ");
+            }
+            print!("   ");
+            len += 1;
+        }
+        
+        if !ascii_buf.is_empty() {
+            println!(" |{}|", ascii_buf);
+        } else {
+            println!();
+        }
+        Ok(())
     }
 }
 
@@ -40,10 +64,11 @@ pub fn print(path: impl AsRef<Path>) -> Result<()> {
         loop {
             let slice = reader.fill_buf().map_err(|_err| HexdumpError::ReadBuffer)?;
             let len = slice.len();
-            slice.print(count);
             if len == 0 {
+                println!("{:08x}", count);
                 break;
             } else {
+                slice.print(count)?;
                 reader.consume(len);
                 count += len;
             }
