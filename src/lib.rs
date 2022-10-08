689e0843c3b241ed96176013cbf9309d4fd06885
Veera <sveera.2001@gmail.com>
2022-10-08 16:23:01 -0400

    Print count and bytes

--- src/lib.rs
+++ src/lib.rs
@@ -12,26 +12,42 @@ pub enum HexdumpError {
 
 type Result<T> = std::result::Result<T, HexdumpError>;
 
-pub fn print(path: impl AsRef<Path>) -> Result<String> {
+trait HexPrinter {
+    fn print(&self, count: usize);
+}
+
+impl HexPrinter for &[u8] {
+    fn print(&self, count: usize) {
+        print!("{:08x} ", count);
+        for (count, byte) in self.iter().enumerate() {
+            if count == 8 {
+                print!(" ");
+            }
+            
+            print!("{:02x} ", byte);
+        }
+        println!();
+    }
+}
+
+pub fn print(path: impl AsRef<Path>) -> Result<()> {
     if path.as_ref().is_dir() {
         Err(HexdumpError::PathIsDir)
     } else {
-        File::open(path)
-            .map_err(|_err| HexdumpError::FileOpen)
-            .and_then(|file| {
-                let mut reader = BufReader::with_capacity(16, file);
-                loop {
-                    let slice = reader.fill_buf()
-                        .map_err(|_err| HexdumpError::ReadBuffer)?;
-                    let len = slice.len();
-                    if len == 0 {
-                        break;
-                    } else {
-                        println!("{:?}", slice);
-                        reader.consume(len);
-                    }
-                }
-                Ok(format!("HELLO"))
-            })
+        let file = File::open(path).map_err(|_err| HexdumpError::FileOpen)?;
+        let mut reader = BufReader::with_capacity(16, file);
+        let mut count = 0;
+        loop {
+            let slice = reader.fill_buf().map_err(|_err| HexdumpError::ReadBuffer)?;
+            let len = slice.len();
+            slice.print(count);
+            if len == 0 {
+                break;
+            } else {
+                reader.consume(len);
+                count += len;
+            }
+        }
+        Ok(())
     }
 }
