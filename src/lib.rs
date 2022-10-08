d741b9913a5b854eef861bffe5c0c6a4c9a18289
Veera <sveera.2001@gmail.com>
2022-10-08 15:32:35 -0400

    Read file into 16 byte containers

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
-use std::{fs::File};
-use std::io::BufReader;
+use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::path::Path;
 
 #[derive(Debug)]
 pub enum HexdumpError {
     PathIsDir,
+    FileOpen,
+    ReadBuffer,
 }
 
 type Result<T> = std::result::Result<T, HexdumpError>;
@@ -14,8 +16,22 @@ pub fn print(path: impl AsRef<Path>) -> Result<String> {
     if path.as_ref().is_dir() {
         Err(HexdumpError::PathIsDir)
     } else {
-        Ok(format!("HELLO"))
+        File::open(path)
+            .map_err(|_err| HexdumpError::FileOpen)
+            .and_then(|file| {
+                let mut reader = BufReader::with_capacity(16, file);
+                loop {
+                    let slice = reader.fill_buf()
+                        .map_err(|_err| HexdumpError::ReadBuffer)?;
+                    let len = slice.len();
+                    if len == 0 {
+                        break;
+                    } else {
+                        println!("{:?}", slice);
+                        reader.consume(len);
+                    }
+                }
+                Ok(format!("HELLO"))
+            })
     }
 }
-    
-    
