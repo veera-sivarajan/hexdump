343038ff0880ea238e73577f367817a70ed4029b
Veera <sveera.2001@gmail.com>
2022-10-09 11:59:28 -0400

    No heap allocations

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,33 @@
-use std::fmt::Write;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
+use std::io;
 
 #[derive(Debug)]
 pub enum HexdumpError {
+    IOError(io::Error),
     PathIsDir,
-    FileOpen,
-    ReadBuffer,
-    WriteBuffer,
+}
+
+impl From<io::Error> for HexdumpError {
+    fn from(err: io::Error) -> Self {
+        HexdumpError::IOError(err)
+    }
 }
 
 type Result<T> = std::result::Result<T, HexdumpError>;
 
 trait HexPrinter {
-    fn print(&self, count: usize) -> Result<()>;
+    fn print(&self, count: usize);
 }
 
 impl HexPrinter for &[u8] {
-    fn print(&self, count: usize) -> Result<()> {
+    fn print(&self, count: usize) {
         print!("{:08x}  ", count); // print the index of first byte in line
-        let mut ascii_buf = String::with_capacity(16);
+        // let mut ascii_buf = String::with_capacity(16);
+        let mut ascii_buf = ['0'; 16];
+        let mut ascii_buf_index = 0;
         for (count, byte) in self.iter().enumerate() {
             if count == 8 {
                 print!(" ");
@@ -29,10 +35,13 @@ impl HexPrinter for &[u8] {
 
             // store perusal format in a buffer
             if *byte >= 32 && *byte <= 126 {
-                write!(ascii_buf, "{}", *byte as char).map_err(|_err| HexdumpError::WriteBuffer)?;
+                // ascii_buf.push(*byte as char);
+                ascii_buf[ascii_buf_index] = *byte as char;
             } else {
-                write!(ascii_buf, ".").map_err(|_err| HexdumpError::WriteBuffer)?;
+                // ascii_buf.push('.');
+                ascii_buf[ascii_buf_index] = '.'; 
             }
+            ascii_buf_index += 1;
 
             print!("{:02x} ", byte);
         }
@@ -49,11 +58,14 @@ impl HexPrinter for &[u8] {
 
         // print perusal format
         if !ascii_buf.is_empty() {
-            println!(" |{}|", ascii_buf);
+            print!(" |");
+            for a in ascii_buf {
+                print!("{a}");
+            }
+            println!("|");
         } else {
             println!();
         }
-        Ok(())
     }
 }
 
@@ -61,24 +73,21 @@ pub fn print(path: impl AsRef<Path>) -> Result<()> {
     if path.as_ref().is_dir() {
         Err(HexdumpError::PathIsDir)
     } else {
-        File::open(path)
-            .map_err(|_err| HexdumpError::FileOpen)
-            .and_then(|file| {
-                let mut reader = BufReader::with_capacity(16, file);
-                let mut count = 0;
-                loop {
-                    let byte_slice = reader.fill_buf().map_err(|_err| HexdumpError::ReadBuffer)?;
-                    let len = byte_slice.len();
-                    if len == 0 {
-                        println!("{:08x}", count);
-                        break;
-                    } else {
-                        byte_slice.print(count)?;
-                        reader.consume(len);
-                        count += len;
-                    }
-                }
-                Ok(())
-            })
+        let file = File::open(path)?;
+        let mut reader = BufReader::with_capacity(16, file);
+        let mut count = 0;
+        loop {
+            let byte_slice = reader.fill_buf()?;
+            let len = byte_slice.len();
+            if len == 0 {
+                println!("{:08x}", count);
+                break;
+            } else {
+                byte_slice.print(count);
+                reader.consume(len);
+                count += len;
+            }
+        }
+        Ok(())
     }
 }
