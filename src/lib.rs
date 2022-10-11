3b27f94c745d7854d7d9b3147c9ac40dc6174f6d
Veera <sveera.2001@gmail.com>
2022-10-10 23:33:01 -0400

    Fix bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use std::io;
 
-#[derive(Copy, Clone)]
 pub struct Hexdump<I, O> {
     input: I,
     output: O,
@@ -15,34 +14,29 @@ where
         Self { input, output }
     }
 
-    fn format_bytes(&mut self, bytes: &[u8]) -> io::Result<()> {
+    fn format_bytes(&mut self, bytes: &[u8], len: usize) -> io::Result<()> {
         let mut ascii_buf = String::with_capacity(16);
         for (count, byte) in bytes.iter().enumerate() {
             if count == 8 {
                 write!(self.output, " ")?;
             }
-
-            // store perusal format in a buffer
-            if *byte >= 32 && *byte <= 126 {
-                ascii_buf.push(*byte as char);
+            
+            if count >= len {
+                write!(self.output, "   ")?;
             } else {
-                ascii_buf.push('.');
-            }
+                // store perusal format in a buffer
+                if *byte >= 32 && *byte <= 126 {
+                    ascii_buf.push(*byte as char);
+                } else {
+                    ascii_buf.push('.');
+                }
 
-            write!(self.output, "{:02x} ", byte)?;
-        }
-
-        // add padding when number of bytes in a line is less than 16
-        for i in bytes.len()..16 {
-            if i == 8 {
-                write!(self.output, " ")?;
+                write!(self.output, "{:02x} ", byte)?;
             }
-            write!(self.output, "   ")?;
         }
 
         // print perusal format
-        writeln!(self.output, " |{}|", ascii_buf)?;
-        Ok(())
+        writeln!(self.output, " |{}|", ascii_buf)
     }
 
     pub fn print(&mut self) -> io::Result<()> {
@@ -56,7 +50,7 @@ where
             } else {
                 // print the index of first byte in line
                 write!(self.output, "{:08x}  ", count)?;
-                self.format_bytes(&buffer)?;
+                self.format_bytes(&buffer, len)?;
                 count += len;
             }
         }
